d124a69ea07b466c2f9a22ab4d100ed2fc7bb388
jfdjkslfid <mr.chaser31@yahoo.com>
2022-05-26 23:11:00 +0200

    1.5
    -OnderdeelController gemaakt
    -OnderdeelVM gemaakt
    -OutfitsTonenGebr view gemaakt

--- OutfitKing/Controllers/OnderdeelController.cs
+++ OutfitKing/Controllers/OnderdeelController.cs
@@ -1,12 +1,105 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using BusnLogicLaag;
+using DALMSSQLSERVER;
+using Microsoft.AspNetCore.Mvc;
+using OutfitKing.Models;
 
 namespace OutfitKing.Controllers
 {
     public class OnderdeelController : Controller
     {
-        public IActionResult Index()
+        private readonly ILogger<OnderdeelController> _logger;
+        private readonly IWebHostEnvironment Environment;
+        public OnderdeelContainer onderdeelContainer = new OnderdeelContainer(new OnderdeelMSSQLDAL());
+
+
+        public OnderdeelController(ILogger<OnderdeelController> logger, IWebHostEnvironment webhostEnvironment)
+        {
+            _logger = logger;
+            Environment = webhostEnvironment;
+        }
+
+        public IActionResult OnderdelenTonenGebr()
+        {
+            try
+            {
+                int? ID = HttpContext.Session.GetInt32("ID");
+                List<Onderdeel> Onderdelen = onderdeelContainer.GetAllOnderdelenVanGebr(ID.Value);
+                return View(Onderdelen);
+            }
+            catch (TemporaryExceptions ex)
+            {
+                return Content($"Er heeft een fout plaatsgevonden, probeer het in 5 minuten nog eens. " + ex.Message);
+            }
+            catch (PermanentExceptions ex)
+            {
+                return Redirect("https://twitter.com/outfitservicestatus");
+            }
+        }
+
+        public IActionResult AlleOnderdelenTonen()
+        {
+            try
+            {
+                List<Onderdeel> Onderdelen = onderdeelContainer.GetAllOnderdelen();
+                return View(Onderdelen);
+            }
+            catch (TemporaryExceptions ex)
+            {
+                return Content($"Er heeft een fout plaatsgevonden, probeer het in 5 minuten nog eens. " + ex.Message);
+            }
+            catch (PermanentExceptions ex)
+            {
+                return Redirect("https://twitter.com/outfitservicestatus");
+            }
+        }
+
+        public IActionResult OnderdeelAanmaken()
         {
             return View();
         }
+
+        [HttpPost]
+        public IActionResult OnderdeelAanmaken(OnderdeelVM onderdeel)
+        {
+            int? ID = HttpContext.Session.GetInt32("ID");
+            try
+            {
+                if (ID == null)
+                {
+                    return Content("U bent niet ingelogd");
+                }
+                else
+                {
+                    string FileNaam = UploadFile(onderdeel);
+                    onderdeelContainer.VoegOnderdeelToe(ID.Value, new Onderdeel(onderdeel.ID, onderdeel.Titel, onderdeel.Prijs, (Onderdeel.OnderdeelCategory)onderdeel.Category, FileNaam));
+                    return RedirectToAction("OutfitsTonenGebr");
+                }
+            }
+            catch (TemporaryExceptions ex)
+            {
+                return Content($"Er heeft een fout plaatsgevonden, probeer het in 5 minuten nog eens. " + ex.Message);
+            }
+            catch (PermanentExceptions ex)
+            {
+                return Redirect("https://twitter.com/outfitservicestatus");
+            }
+        }
+
+        private string UploadFile(OnderdeelVM onderdeel)
+        {
+            string file = null;
+            if (onderdeel.Afbeelding != null)
+            {
+                string uploadDir = Path.Combine(Environment.WebRootPath, "Images");
+                file = Guid.NewGuid().ToString() + "_" + onderdeel.Afbeelding.FileName;
+                string filePath = Path.Combine(uploadDir, file);
+                using (var fileStream = new FileStream(filePath, FileMode.Create))
+                {
+                    onderdeel.Afbeelding.CopyTo(fileStream);
+                }
+            }
+            return file;
+        }
     }
 }
+
--- OutfitKing/Models/OnderdeelVM.cs
+++ OutfitKing/Models/OnderdeelVM.cs
@@ -1,6 +1,26 @@
-﻿namespace OutfitKing.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OutfitKing.Models
 {
     public class OnderdeelVM
     {
+        public enum OnderdeelCategory
+        {
+            Broek,
+            Shirt,
+            Bloes,
+            Schoen,
+            Jurk
+        }
+
+        public int ID { get; set; }
+        [Required]
+        public int Prijs { get; set; }
+        [Required]
+        public string Titel { get; set; }
+        [Required]
+        public OnderdeelCategory Category { get; set; }
+        [Required]
+        public IFormFile Afbeelding { get; set; }
     }
 }
